1f0a3d5fb171c460af2b2309438a6d998fe5b435
JACQUELINE YOUNG <JYOUNG7834@GMAIL.COM>
2020-12-27 15:48:05 -0800

    edits to server.js

--- server/server.js
+++ server/server.js
@@ -1,22 +1,32 @@
 'use strict';
 
-
+//Step 1:  Bring in our modules/dependencies
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+//Step 2:  Set up our application
 const app = express();
 const PORT = process.env.PORT;
 app.use(cors());
 
+//Routes
 
-// app.get('/', (request, response) => {
-//   response.send('Hello World');
-// }
+app.get('/', (request, response) => {
+  response.send('Hello World');
+});
 
+app.get('/location', locationHandler);
 
+app.use('*', (request, response)=> {
+  response.send('404. Sorry!');
+});
 
+function locationHandler(request, response){
+  
+}
 
+//Start our server
 app.listen(PORT, () => {
-  console.log('Now listening on port, ${PORT}');
+  console.log(`Now listening on port, ${PORT}`);
 });
