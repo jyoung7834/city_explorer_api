97ecc3f75bbeaf1759e7f96c582876feda0c4634
JACQUELINE YOUNG <JYOUNG7834@GMAIL.COM>
2020-12-27 22:05:26 -0800

    created function handlers and constructor

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # city_explorer_api
 
-Number and name of feature: ________________________________
+Number and name of feature: December 24, 2020 lab 6 Node, npm, and Express
 
-Estimate of time needed to complete: _______
+Estimate of time needed to complete: 10
 
-Start time: _____
+Start time: 7pm
 
-Finish time: _____
+Finish time: 1:05 pm and still working on it.
 
 Actual time needed to complete: _____
+
+Comments:  When testing node server.js it reads "Now listenin g on port, ${PORT}.  Not communicating with .env file or process.env.PORT is not working. 
--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT;
 app.use(cors());
 
-//Routes
+// Routes
 
 app.get('/', (request, response) => {
   response.send('Hello World');
@@ -18,13 +18,33 @@ app.get('/', (request, response) => {
 
 app.get('/location', locationHandler);
 
-app.use('*', (request, response)=> {
+app.use('*', (request, response) => {
   response.send('404. Sorry!');
 });
 
-function locationHandler(request, response){
-  
+//function Handlers
+function locationHandler(request, response) {
+  //   //This function will do two things:
+  //     // request data from our files:
+  //     // tailor/normalize the data using a constructor
+  //     // respond with the data (show up in the browser) proof of life
+  const location = require('./data/location.json');
+  const city = request.query.city;
+  const locationData = new Location(city, location);
+
+  response.send(locationData);
+
 }
+
+// Constructor
+function Location(city, geoData) {
+  this.search_query = city;
+  this.formatted_query = geoData[0].display_name;
+  this.latitude = geoData[0].lat;
+  this.longitude = geoData[0].lon;
+}
+
+
 
 //Start our server
 app.listen(PORT, () => {
