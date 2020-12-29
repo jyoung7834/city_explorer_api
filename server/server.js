f42293ecb6f62b9642b9608d8d02ee563f9cdb0c
JACQUELINE YOUNG <JYOUNG7834@GMAIL.COM>
2020-12-28 23:28:41 -0800

    Simple serve dunctionality added

--- server/server.js
+++ server/server.js
@@ -1,38 +1,55 @@
 'use strict';
 
-//Step 1:  Bring in our modules/dependencies
+// Step 1: App modules/dependencies
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
 //Step 2:  Set up our application
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.use(cors());
 
 // Routes
 
-app.get('/', (request, response) => {
-  response.send('Hello World');
-});
-
+// app.get('/', (request, response) => {
+//   response.send('Hello World');
+// });
+app.get('/', homeHandler);
+// app.get('/location', locationHandler);
 app.get('/location', locationHandler);
-
-app.use('*', (request, response) => {
-  response.send('404. Sorry!');
-});
+app.get('/weather', weatherHandler);
+// app.use('*', (request, response) => {
+//   response.send('404. Sorry!');
+// });
+app.use('*', notFoundHandler);
 
 //function Handlers
 function locationHandler(request, response) {
-  //   //This function will do two things:
-  //     // request data from our files:
-  //     // tailor/normalize the data using a constructor
-  //     // respond with the data (show up in the browser) proof of life
+  // This function will do two things:
+  // request data from our files:
+  // tailor/normalize the data using a constructor
+  // respond with the data (show up in the browser) proof of life
   const location = require('./data/location.json');
   const city = request.query.city;
   const locationData = new Location(city, location);
 
   response.send(locationData);
+}
+function homeHandler(request, response) {
+  response.send('hello World');
+}
+function notFoundHandler(request, response) {
+  response.send('404, Sorry!');
+}
+function weatherHandler(request, response) {
+  // This function will do two things:
+  // request data from our files:
+  // tailor/normalize the data using a constructor
+  // respond with the data (show up in the browser) proof of life
+  const data = require('./data/weather.json');
+  const weatherArr = [];
+  data.
 
 }
 
@@ -42,6 +59,12 @@ function Location(city, geoData) {
   this.formatted_query = geoData[0].display_name;
   this.latitude = geoData[0].lat;
   this.longitude = geoData[0].lon;
+}
+
+function Restaurant(result) {
+  this.restaurant = 
+  this. cuisines = 
+  this.locality = 
 }
 
 
