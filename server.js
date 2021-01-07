e9453cb8c862259bfe89c0fc4d543d19088c9b9b
JACQUELINE YOUNG <JYOUNG7834@GMAIL.COM>
2021-01-06 19:45:08 -0800

    catching up

--- server.js
+++ server.js
@@ -56,15 +56,15 @@ function Location(value, res) {
 
 function Weather(day) {
   this.forecast = day.summary;
-  this.time = new Date(day.tome * 1000).toString().slice(0,5);
+  this.time = new Date(day.tome * 1000).toString().slice(0, 5);
 }
 function Meetup(meetup) {
   this.link = meetup.link;
   this.name = meetup.group.name;
-  this.creation_date = new Date(meetup.group.created).toString().slice(0,15);
+  this.creation_date = new Date(meetup.group.created).toString().slice(0, 15);
 }
 
-function Movie(movie){
+function Movie(movie) {
   this.title = movie.title;
   this.released_on = movie.release_date;
   this.total_votes = movie.vote_count;
@@ -76,22 +76,72 @@ function Movie(movie){
 
 
 functionYelp(biz) {
-    this.name = biz.name;
-    this.url = biz.url;
-    this.rating = biz.rating;
-    this.price = biz.price;
-    this.image_url = biz.image_url;
+  this.name = biz.name;
+  this.url = biz.url;
+  this.rating = biz.rating;
+  this.price = biz.price;
+  this.image_url = biz.image_url;
 }
 
 
 //  HELPER FUNCTIONS
 
 function handleError(err, response) {
-console.error(err);
-if (res) res.status(500).send('Sorry');
+  console.error(err);
+  if (res) res.status(500).send('Sorry');
 }
 
 
+function getLocation(query) {
+  // Make query string to check for the existence of the location
+  const SQL `SELECT * FROM locations WHERE search_query=$1;`;
+  const values = [query];
 
+  // create the query of the database
+  return client.query(SQL, values)
+    .then(result => {
+      if (result.rowCount > 0 {
+        console.log('SQL working');
+        return result.rows[0];
 
+        //then retrieve information from API
+      } else {
+        console.lot('New API request');
+        const url = `https://majps.googleapis. com//maps/api/geocode/json?adddress=${query}&key=${process.env.GEOCODE_API_KEY}`;
 
+        return superagent.get(url)
+          .then(data => {
+            console.log('From API location');
+
+            //Make err if problem occures with API request
+            if (!data.body.results.length) { throw 'null data' }
+
+            //If so, create an instance of Locatin
+            else {
+              let location = location = new Location(query, data.body.results[0]);
+              console.log('location object from location API', location);
+
+              //make query string to INSERT a new record with the location data
+              let new SQL = 'INSERT INTO location (search_query, formatted_query, latitude, longitude) Values ($1, $2, $3, $4) RETURNING id;';
+              console.log('newValues' newSQL)
+              let newValues = Object.values(location);
+              console.log('neValues', newValues)
+
+              //Place the record to the database
+              return client.query(newSQL, newValues)
+                .then(result => {
+                  console.log('result.rows', result.rows);
+                  // attach the id of the newly created record to the instance of location.
+                  // this will be used to connect the location to the other databases.
+                  console.log('result.rows[0].id', result.rows[0].id)
+                  location.id = result.rows[0].id;
+                  return location;
+                })
+                .catch(console.error);
+            }
+          })
+          .catch(error => console.log('Error in SQL Call'));
+
+      }
+    });
+}   
