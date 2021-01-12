fc3ceaa37195a961b32a15324a9a6836d2cfc66c
JACQUELINE YOUNG <JYOUNG7834@GMAIL.COM>
2021-01-11 21:05:21 -0800

    not caught up

--- server.js
+++ server.js
@@ -22,25 +22,24 @@ client.on('error', err => console.err(err));
 // Routes/end points
 
 app.get('/location', (request, response) => {
-  getLocation(request.query.data)
+  console.log('request', request.query);
+  getLocation(request.query.city)
     .then(location => {
-      console.log('server.js', location);
-      response.send(location)
+      console.log('this is the location', location);
+      response.send(location);
     })
     .catch(error => handleError(error, response));
-})
-
-
-
-app.get('/weather', getWeather);
-//Route for the Movie API
-app.get('/movies', getMovies);
-//ROUTE FOR YELP FUSION API
-app.get('/yelp', getYelp);
-
-app.listen(PORT, () => {
-  console.log(`Now listening on port, ${PORT}`);
 });
+
+
+
+// app.get('/weather', getWeather);
+// //Route for the Movie API
+// app.get('/movies', getMovies);
+// //ROUTE FOR YELP FUSION API
+// app.get('/yelp', getYelp);
+// app.get('*', status404)
+
 
 
 //FUNCTION HANDLERS
@@ -50,55 +49,62 @@ function Location(value, res) {
   this.longitude = res.geometry.location.lng;
 }
 
-function Weather(day) {
-  this.forecast = day.summary;
-  this.time = new Date(day.time * 1000).toString().slice(0, 5);
-}
+// function Weather(day) {
+//   this.forecast = day.summary;
+//   this.time = new Date(day.time * 1000).toString().slice(0, 5);
+// }
 
-function Movie(movie) {
-  this.title = movie.title;
-  this.released_on = movie.release_date;
-  this.total_votes = movie.vote_count;
-  this.average_votes = movie.votes_average;
-  this.popularity = movie.popularity;
-  this.image_url = `https://image.tmdb.org/t/p/original$movie.poster_path}`;
-  this.overview = movie.overview;
-}
+// function Movie(movie) {
+//   this.title = movie.title;
+//   this.released_on = movie.release_date;
+//   this.total_votes = movie.vote_count;
+//   this.average_votes = movie.votes_average;
+//   this.popularity = movie.popularity;
+//   this.image_url = `https://image.tmdb.org/t/p/original$movie.poster_path}`;
+//   this.overview = movie.overview;
+// }
 
 
-function Yelp(biz) {
-  this.name = biz.name;
-  this.url = biz.url;
-  this.rating = biz.rating;
-  this.price = biz.price;
-  this.image_url = biz.image_url;
-}
+// function Yelp(biz) {
+//   this.name = biz.name;
+//   this.url = biz.url;
+//   this.rating = biz.rating;
+//   this.price = biz.price;
+//   this.image_url = biz.image_url;
+// }
 
 
 //  HELPER FUNCTIONS
 
-function handleError(err, response) {
+function handleError(err, res) {
   console.error(err);
-  if (res) res.status(500).send('Sorry');
+  if (res) res.status(404).send('Sorry, not found');
 }
 
 
 function getLocation(query) {
+  console.log('query', query);
   // Make query string to check for the existence of the location
-  const SQL `SELECT * FROM locations WHERE search_query=$1;`;
+  const SQL = `SELECT * FROM locations WHERE search_query=$1;`;
   const values = [query];
 
   // create the query of the database
   return client.query(SQL, values)
     .then(result => {
-      if (result.rowCount > 0 {
+      if (result.rowCount > 0) {
         console.log('SQL working');
         return result.rows[0];
 
         //then retrieve information from API
       } else {
         console.log('New API request');
-        const url = `https://majps.googleapis. com//maps/api/geocode/json?adddress=${query}&key=${process.env.GEOCODE_API_KEY}`;
+        const url =`https://us1.locationiq.com/v1/search.php?key=${key}&format=json&q=Empire%20State%20Building` 
+       
+        // https://us1.locationiq.com/v1/search.php?key=%3CYour_API_Access_Token%3E&format=json&q=Empire%20State%20Building
+        
+        // 'https://us1.location.com/v1/search.php?key=${key}&q${city}&format=json'
+        
+        // `https://maps.googleapis.com/maps/api/geocode/json?city=${query}&key=${process.env.GEOCODE_API_KEY}`;
 
         return superagent.get(url)
           .then(data => {
@@ -113,10 +119,10 @@ function getLocation(query) {
               console.log('location object from location API', location);
 
               //make query string to INSERT a new record with the location data
-              let new SQL = 'INSERT INTO location (search_query, formatted_query, latitude, longitude) Values ($1, $2, $3, $4) RETURNING id;';
-              console.log('newValues' newSQL)
+              let newSQL = 'INSERT INTO locations (search_query, formatted_query, latitude, longitude) Values ($1, $2, $3, $4) RETURNING id;';
+              console.log('newValues', newSQL);
               let newValues = Object.values(location);
-              console.log('neValues', newValues)
+              console.log('neValues', newValues);
 
               //Place the record to the database
               return client.query(newSQL, newValues)
@@ -124,15 +130,23 @@ function getLocation(query) {
                   console.log('result.rows', result.rows);
                   // attach the id of the newly created record to the instance of location.
                   // this will be used to connect the location to the other databases.
-                  console.log('result.rows[0].id', result.rows[0].id)
+                  console.log('result.rows[0].id', result.rows[0].id);
                   location.id = result.rows[0].id;
                   return location;
                 })
                 .catch(console.error);
             }
           })
-          .catch(error => console.log('Error in SQL Call'));
+          .catch(error => console.log('Error in SQL Call', error));
 
       }
     });
 }   
+
+client.connect()
+  .then(( ) => {
+    app.listen(PORT, () => {
+      console.log(`Now listening on port, ${PORT}`);
+    });
+    
+  });
