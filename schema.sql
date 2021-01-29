47f49ace36f5703e5eda5492f960fdc07e224b33
JACQUELINE YOUNG <JYOUNG7834@GMAIL.COM>
2021-01-28 19:43:00 -0800

    schema.sql

--- schema.sql
+++ schema.sql
@@ -10,33 +10,33 @@ CREATE TABLE locations (
 );
 
 CREATE TABLE weather (
---   id SERIAL PRIMARY KEY,
---   forecast VARCHAR(255),
---   time CHAR(15,
---   location_id INTERGER NOT NULL,
---   FOREIGN KEY (location_id) REFERENCES locations (id)
--- );
+  id SERIAL PRIMARY KEY,
+  forecast VARCHAR(255),
+  time CHAR(15),
+  location_id INTERGER NOT NULL,
+  FOREIGN KEY (location_id) REFERENCES locations (id)
+);
 
 -- CREATE TABLE movies (
---   id SERIAL PRIMARY KEY,
---   title VARCHAR(255);
---   released_on VARCHAR(255),
---   total_votes INTEGER NOT NULL,
---   average_votes NUMERIC(7, 2),
---   POPULARITY NUMERIC(3, 1),
---   image_url VARCHAR(255),
---   overvieWs VARCHAR(1000),
---   location_id INTEGER NOT NULL,
---   FOREIGN KEY (location_id) REFERENCES locations (id)
--- );
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255);
+  released_on VARCHAR(255),
+  total_votes INTEGER NOT NULL,
+  average_votes NUMERIC(7, 2),
+  POPULARITY NUMERIC(3, 1),
+  image_url VARCHAR(255),
+  overvieWs VARCHAR(1000),
+  location_id INTEGER NOT NULL,
+  FOREIGN KEY (location_id) REFERENCES locations (id)
+);
 
--- CREATE TABLE restaurants (
---   id SERIAL PRIMARY KEY,
---   name VARCHAR(255),
---   image_url VARCHAR(255);,
---   rating NUMERIC(2,1),
---   price VARCHAR(10),
---   image_url VARCHAR(255),
---   location_id INTEGER NOT NULL,
---   FOREIGN KEY (location_id) REFERENCES location (id)
+CREATE TABLE restaurants (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  image_url VARCHAR(255);,
+  rating NUMERIC(2,1),
+  price VARCHAR(10),
+  image_url VARCHAR(255),
+  location_id INTEGER NOT NULL,
+  FOREIGN KEY (location_id) REFERENCES location (id)
 );
